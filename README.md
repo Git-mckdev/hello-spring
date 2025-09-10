bf2973d9d05bc8cb7009b3209175b8cf1b5559f3
Git-mckdev <mckdev0418@gmail.com>
2025-09-10 19:33:53 +0900

    Static, MVC, API Chapter

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+- Build Chapter
 1. ./gradlew.bat build 또는 ./gradlew.bat clean build 명령어로 .jar 파일 빌드 가능
 2. ./build/libs 경로로 이동하여 java -jar hello-spring-0.0.1-SNAPSHOT.jar 명령어로 빌드된 .jar 파일 실행 가능
+
+- Static, MVC, API Chapter
+1. Static의 경우 파일을 그대로 전송
+2. MVC의 경우 Model, View, Controller 방식으로 나눠 전송
+3. API의 경우 객체를 Json 타입으로 전송
--- src/main/java/com/overflow/hello_spring/controller/HelloController.java
+++ src/main/java/com/overflow/hello_spring/controller/HelloController.java
@@ -3,7 +3,8 @@ package com.overflow.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -13,5 +14,54 @@ public class HelloController {
         model.addAttribute("data", "MC K");
 
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "Hello " + name + "!";
+    }
+
+    /*
+     * @ResponseBody 사용 시 HttpMessageConverter가 동작
+     * 객체의 경우 기본적으로 JsonConverter가 동작 (Json으로 변환)
+     * 문자열의 경우 기본적으로 StringConverter가 동작 (문자열로 변환)
+     */
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name, @RequestParam("age") byte age) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        hello.setAge(age);
+
+        return hello;
+    }
+
+    class Hello {
+        private String name;
+        private byte age;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public byte getAge() {
+            return age;
+        }
+
+        public void setAge(byte age) {
+            this.age = age;
+        }
     }
 }
