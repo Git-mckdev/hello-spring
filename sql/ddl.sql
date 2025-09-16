f98b40b70ed898f231f6b91f540998fa27161124
Git-mckdev <mckdev0418@gmail.com>
2025-09-16 18:29:59 +0900

    Fix ddl.sql

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE `hello-spring` CHARACTER SET utf8mb4;
 
+USE `hello-spring`;
+
 CREATE TABLE `member` (  
     `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
     `name` VARCHAR(255) NOT NULL COMMENT '회원 이름'
